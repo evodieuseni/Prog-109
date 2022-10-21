c2252cfbe46fa79c20213b3199d22030e616c6c9
evodieuseni <114317977+evodieuseni@users.noreply.github.com>
2022-10-21 14:50:30 -0700

    initial push

--- M05/Java/script.js
+++ M05/Java/script.js
@@ -1,1 +1,23 @@
+var table = GetValueFromUser("table");            // Unit of table
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = GetTableContent(table);
 
+function GetValueFromUser(valueType) {
+  var greetingMessage = "Enter the number you want to multiply by";
+  
+  return prompt(greetingMessage);
+}
+
+function GetTableContent(table) {
+  var i = 1;                 // Set counter to 1
+  var msg = '<h2>Multiplication Table</h2>';              // Message  
+{
+    // Do multiplication
+    while (i < 10) {
+      msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+      i++;
+    }
+  }
+  return msg;
+}
